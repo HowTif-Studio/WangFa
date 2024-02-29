1c1f57813a03ae7d35f7c664799a70189baac67c
TiffanytitiWang <124172288+TiffanytitiWang@users.noreply.github.com>
2024-02-29 20:12:45 +0800

    <新增 > 工程實績

--- frontEnd/footer/js/main.js
+++ frontEnd/footer/js/main.js
@@ -7,11 +7,11 @@ const htmlMap = {
               <ul class="left">
                 <li>電話：(02)2694-5249</li>
                 <li>傳真：(02)2693-5200</li>
-                <li>行動電話：0932-022737</li>
+                <li>行動電話：0932-022-737</li>
               </ul>
               <ul class="right">
                 <li>E-Mail：wang.fa@msa.hinet.net</li>
-                <li>地址：新北市汐止區福德一路141巷11號2樓</li>
+                <li>地址：新北市汐止區福德一路 141 巷 11 號 2 樓</li>
               </ul>
            </div>
            <div class="copyRight">COPYRIGHT-POWER BY WANGFA</div>`
