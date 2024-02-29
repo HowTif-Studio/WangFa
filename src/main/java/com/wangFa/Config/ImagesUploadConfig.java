2a82acd7fa0d49f8a172df48d69fd1f14f3285b8
Howard0826 <hd40418@gmail.com>
2024-02-29 23:05:36 +0800

    [修正] javascript, css 無法讀取修復

--- src/main/java/com/wangFa/Config/ImagesUploadConfig.java
+++ src/main/java/com/wangFa/Config/ImagesUploadConfig.java
@@ -14,7 +14,9 @@ public class ImagesUploadConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 將讀取圖片路徑 /image/** 映射到真實的圖片存放路徑上
-        registry.addResourceHandler("/image/**").addResourceLocations("file:"+filePath);
+        registry.addResourceHandler("/javascript/**").addResourceLocations("classpath:/static/javascript/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/image/**").addResourceLocations("file:" + filePath);
     }
 }
 
