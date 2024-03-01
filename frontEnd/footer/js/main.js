748d99de444a0ce7d41c79a11788a0abbb1a49f0
TiffanytitiWang <124172288+TiffanytitiWang@users.noreply.github.com>
2024-03-01 15:35:36 +0800

    <新增 > 前端調整

--- frontEnd/footer/js/main.js
+++ frontEnd/footer/js/main.js
@@ -5,13 +5,13 @@ const htmlMap = {
     main: `<div class="info">
               <div class="logo"></div>
               <ul class="left">
-                <li>電話：(02)2694-5249</li>
-                <li>傳真：(02)2693-5200</li>
-                <li>行動電話：0932-022-737</li>
+                <li>電話 ：(02)2694-5249</li>
+                <li>傳真 ：(02)2693-5200</li>
+                <li>行動電話 ：0932-022-737</li>
               </ul>
               <ul class="right">
-                <li>E-Mail：wang.fa@msa.hinet.net</li>
-                <li>地址：新北市汐止區福德一路 141 巷 11 號 2 樓</li>
+                <li>E-Mail ：wang.fa@msa.hinet.net</li>
+                <li>地址 ：新北市汐止區福德一路 141 巷 11 號 2 樓</li>
               </ul>
            </div>
            <div class="copyRight">COPYRIGHT-POWER BY WANGFA</div>`
