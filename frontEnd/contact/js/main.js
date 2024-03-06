cc2ded4f5c0f52585e0a5fe107072ac1c874561e
TiffanytitiWang <124172288+TiffanytitiWang@users.noreply.github.com>
2024-03-07 00:32:47 +0800

    < 新增 > mailto 功能

--- frontEnd/contact/js/main.js
+++ frontEnd/contact/js/main.js
@@ -3,38 +3,41 @@ import $ from "jquery";
 import {init as header} from "../../header/js/main";
 import {init as footer} from "../../footer/js/main";
 
+
+
+const bindSentEvent= () => {
+    $('#sent').on('click', (e)=>{
+        console.log('click!!')
+        getEmailData();
+    });
+}
+
+    //按下傳送按鈕後執行
+    function getEmailData() {
+        let $name_input = $('#name_input');
+        let $email_input = $('#email_input');
+        let $phone_input = $('#phone_input');
+        let $content_input = $('#content_input');
+        let $mailTo = $('#mailTo');
+        let to = "wang.fa@msa.hinet.net"; // 寫死的傳送對象
+        let name = $name_input.val(); // 讀取 ID 物件中的值
+        let email = $email_input.val();
+        let tel = $phone_input.val();
+        let subject = "王發機械：問題詢問";
+
+//把資料都塞到 mail body 中
+        let body = "" + $content_input.val() + '%0A%0A%0A';//%0A是換行 換了三行
+        body += "From：" + name + '%0A';
+        body += "Email：" + email + '%0A';
+        body += "Tel：" + tel;
+//傳送的主要程式碼
+        $mailTo.attr('href', "mailto:" + to + "?subject=" + subject + "&body=" + body);
+        $mailTo[0].click();
+    }
+
 $(window).on('load', function () {
     header();
     footer();
-    init();
+    bindSentEvent();
 });
-
-
-    //宣告預設表單內容為空
-    var initSubject = '', initBody = '';
-
-    //按下傳送按鈕後執行
-    function sentEmail() {
-        var to = "wang.fa@msa.hinet.net"; // 寫死的傳送對象
-        var name = name_input.value; // 讀取 ID 物件中的值
-        var email = email_input.value;
-        var tel = phone_input.value;
-        var subject = "王發機械：問題詢問";
-
-//把資料都塞到 mail body 中
-        var body = "" + content_input.value + '%0A%0A%0A';//%0A是換行 換了三行
-        body += "From：" + name_input.value + '%0A';
-        body += "Email：" + email_input.value + '%0A';
-        body += "Tel：" + phone_input.value;
-//傳送的主要程式碼
-        mailTo.href = "mailto:" + to + "?subject=" + subject + "&body=" + body;
-        mailTo.click();
-    }
-
-    //onload
-    function init() {
-        subText.value = initSubject;
-        toText.value = initTo;
-        bodyText.value = initBody;
-    }
 
